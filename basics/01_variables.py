3442326ac3f9155e6a4c41746b61f7ea49999ba3
Manish Singh <manishsingh0434@gmail.com>
2026-07-31 22:03:02 +0530

    practice variables

--- basics/01_variables.py
+++ basics/01_variables.py
@@ -5,3 +5,40 @@ city = "Delhi"
 print(name)
 print(course)
 print(city)
+
+
+full_name = "Manish Singh"
+age = 22
+favorite_programming_language = "python"
+
+print(full_name)
+print(age)
+print(favorite_programming_language)
+
+company_name = "network.nuts"
+city = "Delhi"
+country = "India"
+
+Laptop_brand = "asus"
+RAM_gb = ("8GB")
+Processor = ("intel i7")
+
+print(Laptop_brand)
+print(RAM_gb)
+print(Processor)
+
+name = "Manish"
+course = "Pyhton for DevOps"
+duration = "3 months"
+
+print("Name:",name)
+print("Course:",course)
+print("Duration:",duration)
+
+city = "Delhi"
+country = "India"
+pincode = 110001
+
+print("City:", city)
+print("Country:", country)
+print("Pincode:", pincode)
