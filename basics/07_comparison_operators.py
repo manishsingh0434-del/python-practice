e1694e1c43baa275fd26875c3081aef9802c3cd6
Manish Singh <manishsingh0434@gmail.com>
2026-08-07 19:51:22 +0530

    Practice of comparison Operators

---
+++ basics/07_comparison_operators.py
@@ -0,0 +1,17 @@
+num1 = 20
+num2 = 15
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2)
+
+
+age = int(input("Enter you age: "))
+print(age >= 18)
+print(age < 18)
+
+password = input("Enter password: ")
+print(password == "admin123")
