0a79ce3a2bb7bb6f1e5971d7c76d9f41953d888a
Manish Singh <manishsingh0434@gmail.com>
2026-08-09 21:26:38 +0530

    Practice of if else in python

---
+++ basics/08_if_else.py
@@ -0,0 +1,11 @@
+age = 15
+if age >= 18:
+    print("you are an adult")
+else:
+    print("you are a minor")
+
+age = int(input("Enter your age:"))
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are an minor")
