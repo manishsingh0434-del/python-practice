2d742dd89266dfe2bf294b05de5f73a1f2a5356e
Manish Singh <manishsingh0434@gmail.com>
2026-08-11 17:46:58 +0530

    Practice nested if

---
+++ basics/10_nested_if.py
@@ -0,0 +1,20 @@
+age = int(input("Enter your age:"))
+has_id = input("Do you have an ID? (yes/no):")
+if age >= 18:
+    if has_id == "yes":
+        print("You can enter")
+    else:
+        print("You need an ID")
+else:
+    print("You are an under 18")
+
+
+marks = int(input("Enter your marks:"))
+
+if marks <= 20:
+    if marks >= 80:
+        print("Excellent")
+    else:
+        print("you passed")
+else:
+    print("you failed")
