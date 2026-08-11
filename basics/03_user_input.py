9d5e2bf559d74e64301195453e028cc4fb7751be
Manish Singh <manishsingh0434@gmail.com>
2026-08-11 20:52:17 +0530

    practice of user input

--- basics/03_user_input.py
+++ basics/03_user_input.py
@@ -3,7 +3,7 @@
 
 
 
-brand_name = input("enter your brand name:")
+brand_name = input("Enter your brand name:")
 origin_city = input("Enter origin city:")
 print("Brand Name:", brand_name)
 print("Origin City:", origin_city)
