043f88d546c3b60060350a03e8f74c5d9901f613
Manish Singh <manishsingh0434@gmail.com>
2026-08-15 17:55:42 +0530

    Practice of while loop

---
+++ basics/17_while_practice.py
@@ -0,0 +1,17 @@
+while True:
+    number = int(input("Enter a number:"))
+
+    if number == 0:
+        break
+    print(number)
+
+
+
+total = 0
+while True:
+    number = int(input("Enter a number: "))
+
+    if number == 0:
+        break
+    total = total + number
+    print("tota:", total)
