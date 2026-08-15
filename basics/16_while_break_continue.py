177535cd0353c6e02a29520b7ff5438ad7015e44
Manish Singh <manishsingh0434@gmail.com>
2026-08-15 17:41:51 +0530

    Practice of while break continue

---
+++ basics/16_while_break_continue.py
@@ -0,0 +1,43 @@
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# i = 1
+# while i <= 5:
+#     if i == 3:
+#         break
+
+#     print(i)
+#     i += 1
+
+# i = 1
+# while i <= 4:
+#     print(i)
+#     i += 1
+
+
+# i = 1
+# while i <= 7:
+#     if i == 4:
+#         break
+#     print(i)
+#     i += 1
+
+
+# i = 0 
+# while i < 5:
+#     i += 1
+
+#     if i == 3:
+#         continue
+#     print(i)
+
+
+i = 0
+while i < 10:
+    i += 1
+
+    if i == 5:
+        continue
+    print(i)
